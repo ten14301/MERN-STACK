0fe607052fca205c69750a81f72b0aed65600fd6
ten14301 <ten077710@gmail.com>
2023-06-07 01:15:21 +0700

    views

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const {MongoClient} = require('mongodb')
 const app = express();
+let db 
 
-app.get("/",(req,res) => {
-    res.send("Welcome to HomePage")
+app.set("view engine", "ejs")
+app.set("views","./views")
+app.get("/", async (req,res) => {
+    const allusers = await db.collection('User').find().toArray()
+    res.render("home", { allusers})
 })
 
 
@@ -11,5 +15,12 @@ app.get("/admin",(req,res) => {
     res.send("welcome Admin")
 })
 
+async function start(){
+    const client = new MongoClient("mongodb://admin:admin@localhost:27017/Mern?&authSource=admin")
+    await client.connect()
+    db = client.db()
+    app.listen(3000);
+}
 
-app.listen(3000);
+start()
+
